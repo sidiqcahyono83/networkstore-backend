361c999fd9052dd5b0ba90192c93b69e26f0a342
cahyono muslim sidiq <cdq.cahyono@gmail.com>
2024-07-16 19:20:19 +0700

    fix docker.yaml,delet shoopingCartItem

--- prisma/migrations/20240716111818_init_ke_6/migration.sql
+++ prisma/migrations/20240716111818_init_ke_6/migration.sql
@@ -45,7 +45,6 @@ CREATE TABLE "ShoppingCart" (
 CREATE TABLE "ShoppingCartItem" (
     "id" TEXT NOT NULL,
     "cartId" TEXT NOT NULL,
-    "productId" TEXT NOT NULL,
     "quantity" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -67,6 +66,3 @@ ALTER TABLE "ShoppingCart" ADD CONSTRAINT "ShoppingCart_userId_fkey" FOREIGN KEY
 
 -- AddForeignKey
 ALTER TABLE "ShoppingCartItem" ADD CONSTRAINT "ShoppingCartItem_cartId_fkey" FOREIGN KEY ("cartId") REFERENCES "ShoppingCart"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "ShoppingCartItem" ADD CONSTRAINT "ShoppingCartItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
