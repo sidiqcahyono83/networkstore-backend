2bae68a80c85c3a3fad12a9a27264fddf4d402e4
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-07-21 14:11:57 +0700

    fix typo

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,24 +1,29 @@
 export const products = [
-  {
-    name: "Routerboard CCR2004-1G-12S+2XS",
-    description:
-      "The Connectivity Router - your best companion when it comes to SFP, SFP+ and SFP28 management! 1, 10 and 25 Gbps ports in a single device to make your life easier",
-    price: 8240000,
-    stock: 9,
-    category: "Router",
-    imageUrl: "/CCR3.png",
-    createdAt: "2024-07-15T07:04:33.004Z",
-    updatedAt: "2024-07-15T07:02:31.207Z",
-  },
-  {
-    name: "RB4011iGS+RM",
-    description:
-      "10xGigabit port router with a Quad-core 1.4Ghz CPU 1GB RAM, SFP+ 10Gbps cage and desktop case with rack ears",
-    price: 3500000,
-    stock: 7,
-    category: "Router",
-    imageUrl: "/4011.png",
-    createdAt: "2024-07-15T07:06:07.865Z",
-    updatedAt: "2024-07-15T07:04:36.043Z",
-  },
+	{
+		name: "Routerboard CCR2004-1G-12S+2XS",
+		description:
+			"The Router best companion when it comes to SFP, SFP+ and SFP28 management! 1",
+		price: 8240000,
+		stock: 9,
+		category: "Router",
+		imageUrl: "/CCR3.png",
+	},
+	{
+		name: "RB4011iGS+RM",
+		description:
+			"10xGigabit port, Quad-core 1.4Ghz CPU 1GB RAM, SFP+ 10Gbps",
+		price: 3500000,
+		stock: 7,
+		category: "Router",
+		imageUrl: "/4011.png",
+	},
+	{
+		name: "CCR 224",
+		description:
+			"10xGigabit port, Quad-core 1.4Ghz CPU 1GB RAM, SFP+ 10Gbps",
+		price: 3500000,
+		stock: 7,
+		category: "Router",
+		imageUrl: "/2024.png",
+	},
 ];
--- src/routes/product.tsx
+++ src/routes/product.tsx
@@ -4,110 +4,110 @@ import prisma from "../lib/prisma";
 export const app = new Hono();
 
 app.get("/", async (c) => {
-  try {
-    const allProduct = await prisma.product.findMany();
-    return c.json(
-      {
-        success: true,
-        massage: "List data product",
-        data: allProduct,
-      },
-      200
-    );
-  } catch (error) {
-    console.error(`Error get movies : ${error}`);
-  }
+	try {
+		const allProduct = await prisma.product.findMany();
+		return c.json(
+			{
+				success: true,
+				message: "List data product",
+				data: allProduct,
+			},
+			200
+		);
+	} catch (error) {
+		console.error(`Error get movies : ${error}`);
+	}
 });
 
 app.post("/", async (c) => {
-  try {
-    const body = await c.req.json();
-    // const ShoppingCartItemData = [];
-    // for (let i = 0; i < body.ShoppingCartItem.length; i++) {
-    //   ShoppingCartItemData.push({
-    //     where: { id: body.ShoppingCartItem[i] },
-    //     create: { id: body.ShoppingCartItem[i] },
-    //   });
-    // }
+	try {
+		const body = await c.req.json();
+		// const ShoppingCartItemData = [];
+		// for (let i = 0; i < body.ShoppingCartItem.length; i++) {
+		//   ShoppingCartItemData.push({
+		//     where: { id: body.ShoppingCartItem[i] },
+		//     create: { id: body.ShoppingCartItem[i] },
+		//   });
+		// }
 
-    const newProduct = await prisma.product.create({
-      data: {
-        name: String(body.name),
-        description: String(body.description),
-        price: Number(body.price),
-        stock: Number(body.stock),
-        category: String(body.category),
-        imageUrl: String(body.imageUrl),
-        // ShoppingCartItem: {
-        //   connectOrCreate: ShoppingCartItemData,
-        // },
-      },
-    });
+		const newProduct = await prisma.product.create({
+			data: {
+				name: String(body.name),
+				description: String(body.description),
+				price: Number(body.price),
+				stock: Number(body.stock),
+				category: String(body.category),
+				imageUrl: String(body.imageUrl),
+				// ShoppingCartItem: {
+				//   connectOrCreate: ShoppingCartItemData,
+				// },
+			},
+		});
 
-    return c.json(newProduct);
-  } catch (error) {
-    console.error(`Error get product : ${error}`);
-  }
+		return c.json(newProduct);
+	} catch (error) {
+		console.error(`Error get product : ${error}`);
+	}
 });
 
 app.get("/:id", async (c) => {
-  try {
-    const id = c.req.param("id");
-    const product = await prisma.product.findUnique({
-      where: { id: id },
-    });
-    if (!product) {
-      return c.json(
-        {
-          success: false,
-          massage: `product not found!`,
-        },
-        404
-      );
-    }
-    return c.json({
-      success: true,
-      message: `Detail product ${product.name}`,
-      data: product,
-    });
-  } catch (error) {
-    console.error(`Error get product by id : ${error}`);
-  }
+	try {
+		const id = c.req.param("id");
+		const product = await prisma.product.findUnique({
+			where: { id: id },
+		});
+		if (!product) {
+			return c.json(
+				{
+					success: false,
+					message: `product not found!`,
+				},
+				404
+			);
+		}
+		return c.json({
+			success: true,
+			message: `Detail product ${product.name}`,
+			data: product,
+		});
+	} catch (error) {
+		console.error(`Error get product by id : ${error}`);
+	}
 });
 
 app.delete("/:id", async (c) => {
-  const id = c.req.param("id");
-  const product = await prisma.product.delete({
-    where: { id: id },
-  });
-  if (!id) {
-    return c.json({ message: "products Not Found" });
-  }
-  return c.json(`product by name ${product.name} deleted`);
+	const id = c.req.param("id");
+	const product = await prisma.product.delete({
+		where: { id: id },
+	});
+	if (!id) {
+		return c.json({ message: "products Not Found" });
+	}
+	return c.json(`product by name ${product.name} deleted`);
 });
 
 app.put("/:id", async (c) => {
-  try {
-    const id = c.req.param("id");
-    const body = await c.req.json();
-    if (!id) {
-      return c.json({ massage: `product not found`, Status: 404 });
-    }
-    const newProduct = await prisma.product.update({
-      where: { id },
-      data: {
-        name: String(body.name),
-        description: String(body.description),
-        price: Number(body.price),
-        stock: Number(body.stock),
-        category: String(body.category),
-        imageUrl: String(body.imageUrl),
-      },
-    });
-    return c.json(newProduct);
-  } catch (error) {
-    console.error(`Error product : ${error}`);
-  }
+	try {
+		const id = c.req.param("id");
+		const body = await c.req.json();
+		if (!id) {
+			return c.json({ message: `product not found`, Status: 404 });
+		}
+		const newProduct = await prisma.product.update({
+			where: { id },
+			data: {
+				name: String(body.name),
+				description: String(body.description),
+				price: Number(body.price),
+				stock: Number(body.stock),
+				category: String(body.category),
+				imageUrl: String(body.imageUrl),
+			},
+		});
+		return c.json(newProduct);
+	} catch (error) {
+		console.error(`Error product : ${error}`);
+	}
 });
 
 export default app;
