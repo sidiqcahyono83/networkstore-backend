d85e7d47d9840febee1d06aa3c6e1237136393be
M Haidar Hanif <git@mhaidarhanif.com>
2024-07-29 10:57:32 +0700

    fix: hono app type issue

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,28 @@ import loginRoute from "./routes/login";
 import cartRoute from "./routes/shopingcart";
 import shoppingCartRoute from "./routes/shppingCartItem";
 
-const app = new Hono();
+type Bindings = {
+  TOKEN: string;
+};
+
+type Variables = {
+  user: {
+    id: string;
+  };
+};
+
+export type HonoApp = { Bindings: Bindings; Variables: Variables };
+
+const app = new Hono<HonoApp>();
 
 app.use("*", cors());
 app.get("/", (c) => {
-	return c.json({
-		message: "NetworkStore API",
-		products_URL: "/products",
-		users_URL: "/users",
-		cart_URL: "/cart",
-	});
+  return c.json({
+    message: "NetworkStore API",
+    products_URL: "/products",
+    users_URL: "/users",
+    cart_URL: "/cart",
+  });
 });
 
 app.route("/products", productRoute);
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,98 +1,101 @@
 import { Hono } from "hono";
+import { z } from "zod";
+
 import prisma from "../lib/prisma";
 import { zValidator } from "@hono/zod-validator";
-import { z } from "zod";
 import { verifyPassword } from "../lib/password";
 import { createToken } from "../lib/jwt";
 import { checkUserToken } from "../midleware/cekUserToken";
+import { HonoApp } from "../index";
 
-export const app = new Hono();
+export const app = new Hono<HonoApp>();
 
 app.post(
-	"/auth",
-	zValidator(
-		"json",
-		z.object({
-			username: z.string(),
-			password: z.string(),
-		})
-	),
-	async (c) => {
-		const body = c.req.valid("json");
+  "/auth",
+  zValidator(
+    "json",
+    z.object({
+      username: z.string(),
+      password: z.string(),
+    })
+  ),
+  async (c) => {
+    const body = c.req.valid("json");
 
-		const foundUser = await prisma.user.findUnique({
-			where: { username: body.username },
-			include: { password: { select: { hash: true } } },
-		});
+    const foundUser = await prisma.user.findUnique({
+      where: { username: body.username },
+      include: { password: { select: { hash: true } } },
+    });
 
-		if (!foundUser) {
-			c.status(404);
-			return c.json({ message: "Cannot login because user not found" });
-		}
+    if (!foundUser) {
+      c.status(404);
+      return c.json({ message: "Cannot login because user not found" });
+    }
 
-		if (!foundUser?.password?.hash) {
-			c.status(400);
-			return c.json({
-				message: "Cannot login because user doesn't have a password",
-			});
-		}
+    if (!foundUser?.password?.hash) {
+      c.status(400);
+      return c.json({
+        message: "Cannot login because user doesn't have a password",
+      });
+    }
 
-		const validPassword = await verifyPassword(
-			foundUser.password.hash,
-			body.password
-		);
+    const validPassword = await verifyPassword(
+      foundUser.password.hash,
+      body.password
+    );
 
-		if (!validPassword) {
-			c.status(400);
-			return c.json({
-				message: "Password incorrect",
-			});
-		}
+    if (!validPassword) {
+      c.status(400);
+      return c.json({
+        message: "Password incorrect",
+      });
+    }
 
-		const token = await createToken(foundUser.id);
+    const token = await createToken(foundUser.id);
 
-		if (!token) {
-			c.status(400);
-			return c.json({ message: "Token failed to create" });
-		}
+    if (!token) {
+      c.status(400);
+      return c.json({ message: "Token failed to create" });
+    }
 
-		return c.json({
-			message: "Login successful",
-			token,
-		});
-	}
+    return c.json({
+      message: "Login successful",
+      token,
+    });
+  }
 );
 
 app.get("/auth/me", checkUserToken(), async (c) => {
-	const user = c.get("user");
-	const userData = await prisma.user.findUnique({
-		where: { id: user.id },
-		select: {
-			id: true,
-			username: true,
-			createdAt: true,
-			updatedAt: true,
-			cart: {
-				select: {
-					id: true,
-					items: true,
-				},
-			},
-		},
-	});
+  const user = c.get("user");
 
-	return c.json({
-		message: "User data",
-		user: userData,
-	});
+  const userData = await prisma.user.findUnique({
+    where: { id: user.id },
+    select: {
+      id: true,
+      username: true,
+      createdAt: true,
+      updatedAt: true,
+      cart: {
+        select: {
+          id: true,
+          items: true,
+        },
+      },
+    },
+  });
+
+  return c.json({
+    message: "User data",
+    user: userData,
+  });
 });
 
 app.get("/auth/logout", checkUserToken(), async (c) => {
-	// Note: might be unnecessary since this is token-based auth
-	// We can just remove the token on the client or frontend
-	return c.json({
-		message: "Logout",
-	});
+  // Note: might be unnecessary since this is token-based auth
+  // We can just remove the token on the client or frontend
+  return c.json({
+    message: "Logout",
+  });
 });
 
 // app.get("/cart", checkUserToken(), async (c) => {
